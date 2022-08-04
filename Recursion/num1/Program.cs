a4ae29890d2540d67fa1d4cf0b68c382de242ac6
masturah45 <adesanyamasturah@gmail.com>
2022-08-04 13:11:55 +0100

    added recursion

---
+++ Recursion/num1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Program
+    {
+         static void Loops(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                foreach (int element in arr) Console.Write("{0} ", element);
+                Console.WriteLine();
+            }
+            else
+                for (int i = 1; i <= arr.Length; i++)
+                {
+                    arr[index] = i;
+                    Loops(arr, index + 1);
+                }
+        }   
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");            
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Loops(arr, 0);
+        }
+    }
+}
---
+++ Recursion/num2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void GetCombinations(int[] arr, int index, int start, int end)
+        {
+            if (index >= arr.Length)
+            {
+                Console.Write("(");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write("), ");
+            }
+            else
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    GetCombinations(arr, index + 1, i, end);
+                }
+        }
+        static void Main(string[] args)
+        {
+           Console.Write("Enter your number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            int k = int.Parse(Console.ReadLine());
+            int[] arr = new int[k];
+            GetCombinations(arr, 0, 1, n); 
+        }
+    }
+}
